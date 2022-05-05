23fbacd7c4aee7e6adf573a17a74062a005e0a89
bzhao <Bin.Zhao@nasa.gov>
2022-05-05 19:33:47 -0400

    kmt and bathymetry file output done

--- mom2cice6.py
+++ mom2cice6.py
@@ -22,7 +22,22 @@ grid_out = 'cice6_grid.nc'
 kmt_out = 'cice6_kmt.nc'
 bathy_out = 'cice6_global.bathy.nc'
 
+ncfilein = Dataset(topo_file, 'r', format='NETCDF4')
+depth  = ncfilein.variables['depth'][:]
+#ncfile.close()
+for dimo in ncfilein.dimensions.values():
+   print(type(dimo))
+ncfilein.close()
 
+
+ncfileout = Dataset(bathy_out, 'w', format='NETCDF4')
+ni = ncfileout.createDimension("ni", depth.shape[1])
+nj = ncfileout.createDimension("nj", depth.shape[0])
+bathy = ncfileout.createVariable("Bathymetry", "f4",("nj", "ni",))
+bathy.long_name = "ocean bathymetry for grounding scheme"
+bathy.units = "m"
+bathy[:] = depth
+ncfileout.close()
 
 ncfile = Dataset(mapl_tripole, 'r', format='NETCDF4')
 tlat  = ncfile.variables['lat_centers'][:]
@@ -36,9 +51,16 @@ huw   = ncfile.variables['huw'][:]
 angle = ncfile.variables['angleu'][:]
 mask  = ncfile.variables['mask'][:]
 ncfile.close()
-ncfile = Dataset(topo_file, 'r', format='NETCDF4')
-depth  = ncfile.variables['depth'][:]
-ncfile.close()
+
+
+ncfileout = Dataset(kmt_out, 'w', format='NETCDF4')
+x = ncfileout.createDimension("x", depth.shape[1])
+y = ncfileout.createDimension("y", depth.shape[0])
+kmt = ncfileout.createVariable("kmt", "f4",("y", "x",))
+kmt.units = "m"
+mask[mask > 0.5] = 1000.0
+kmt[:] = mask
+ncfileout.close()
 
 
 
